d93aec69653ca77aa1ceabb8b16d240c94616e04
Sean Martin <sean.martin@gmail.com>
2018-09-18 20:59:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,9 @@ end
 def batch_badge_creator(names)
   names.map { |name| badge_maker(name)}
 end
+
+def assign_rooms(speakers)
+  speakers.map_with_index do |speaker, i| 
+    "Hello, #{speaker}! You'll be assigned to room #{i}!"
+  end
+end
